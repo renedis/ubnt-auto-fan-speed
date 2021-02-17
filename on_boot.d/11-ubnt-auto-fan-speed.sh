907033d374821ab554d770cc5318af0ac27898b7
renedis <renedis@hotmail.com>
2021-02-17 17:30:41 +0100

    Added explanation

--- on_boot.d/11-ubnt-auto-fan-speed.sh
+++ on_boot.d/11-ubnt-auto-fan-speed.sh
@@ -5,8 +5,10 @@
 ## MODE CONFIG FOR AUTOMATIC FAN CONTROL
 ##
 ##  0 = OFF     (Fans runnning at full speed)
-##  1 = MANUAL  (Fan speed will explicitly be set)
+##  1 = MANUAL  (Fan speed will explicitly be set at own set values)
 ##  2 = AUTO    (Controller will adjust PWM duty cycle depending on temperature)
+##
+##  This script does not check for a HDD/SSD!
 ##
 ##########
 
@@ -35,20 +37,27 @@ echo ${UBIOS_HDD_FAN_MODE} >/sys/class/hwmon/hwmon0/device/pwm1_enable
 
 ##########################
 ##########################
-#### CONFIG VARIABLES ####
+##### CONFIG VALUES ######
 ##########################
 ##########################
 
 
 ####################################################################
-## Setting of fan speeds / PWM duty cycles #########################
-## Value of environment variable = 2.56 x intended Duty Cycle [%] ##
+########## Setting of fan speeds / PWM duty cycles #################
+################## Example: 60 x 0,392 = 23,53% ####################
 ####################################################################
 
-UBIOS_CPU_MINIMAL_DUTY_FAN_PWM='60'        # default 128, 106 = 2.400 rpm
-UBIOS_HDD_MINIMAL_DUTY_FAN_PWM='0'         # default 128, 70 = 1.950 rpm
-UBIOS_CPU_MAXIMUM_DUTY_FAN_PWM='255'       # default 255
-UBIOS_HDD_MAXIMUM_DUTY_FAN_PWM='255'       # default 255
+#####################################################################################
+## Explained in a break-down:                                                      ##
+## - 60 is the value set for the thermal chip                                      ##
+## - 0,392 is the value to calculate to percentage (this value is always the same) ##
+## - 23,53% is the outcome value in percentage that the fan will run on            ##
+#####################################################################################
+
+UBIOS_CPU_MINIMAL_DUTY_FAN_PWM='60'        # default 128, this sets the minimal RPM for the CPU fan
+UBIOS_HDD_MINIMAL_DUTY_FAN_PWM='0'         # default 128, this sets the minimal RPM for the HDD fan
+UBIOS_CPU_MAXIMUM_DUTY_FAN_PWM='255'       # default 255, this sets the maximum RPM for the CPU fan
+UBIOS_HDD_MAXIMUM_DUTY_FAN_PWM='255'       # default 255, this sets the miximum RPM for the CPU fan
 
 # Setting minimal CPU fan duty
 echo ${UBIOS_CPU_MINIMAL_DUTY_FAN_PWM} >/sys/class/hwmon/hwmon0/device/pwm2_auto_point1_pwm
@@ -62,16 +71,17 @@ echo ${UBIOS_CPU_MAXIMUM_DUTY_FAN_PWM} >/sys/class/hwmon/hwmon0/device/pwm2_auto
 # Setting maximum HDD fan duty
 echo ${UBIOS_HDD_MAXIMUM_DUTY_FAN_PWM} >/sys/class/hwmon/hwmon0/device/pwm1_auto_point2_pwm
 
-#############################
-## Setting of temperatures ##
-#############################
+####################################
+##### Setting of temperatures ######
+## 40000 equals 40 degree celsius ##
+####################################
 
-UBIOS_CPU_MINIMAL_TEMP='40000'       # default 45000
-UBIOS_HDD_MINIMAL_TEMP='40000'       # default 90000
-UBIOS_BRD_MINIMAL_TEMP='40000'       # default 90000
-UBIOS_CPU_MAXIMUM_TEMP='66000'       # default 72000
-UBIOS_HDD_MAXIMUM_TEMP='66000'       # default 112000
-UBIOS_BRD_MAXIMUM_TEMP='66000'       # default 112000
+UBIOS_CPU_MINIMAL_TEMP='40000'       # default 45000, 45 degree celsius
+UBIOS_HDD_MINIMAL_TEMP='40000'       # default 90000, 90 degree celsius
+UBIOS_BRD_MINIMAL_TEMP='40000'       # default 90000, 90 degree celsius
+UBIOS_CPU_MAXIMUM_TEMP='66000'       # default 72000, 72 degree celsius
+UBIOS_HDD_MAXIMUM_TEMP='66000'       # default 112000, 112 degree celsius
+UBIOS_BRD_MAXIMUM_TEMP='66000'       # default 112000, 112 degree celsius
 
 # Setting minimal CPU temp
 echo ${UBIOS_CPU_MINIMAL_TEMP} >/sys/class/hwmon/hwmon0/device/temp1_auto_point1_temp
